63ff46ab1dd6bad9fd674b7ca4fb87e3a9704a64
Sumio K <tap.miwo@gmail.com>
2022-11-07 22:16:59 +0900

    before function

--- base.py
+++ base.py
@@ -1,5 +1,39 @@
 # 出力
-print("hello world!")
+# print("hello world!")
+# print(256)
 
-print(256)
 
+# 演算
+# print(1+1)
+# print(1-1)
+# print(1*1)
+# print(1/1)
+# print(1%1)
+
+
+# 変数
+hello = "Hello, world!"
+hello_length = 5
+hello_times = 3.5
+hello_greet = True
+
+  # 変数の型を調べる
+# print(type(hello))
+# print(type(hello_length))
+# print(type(hello_times))
+# print(type(hello_greet))
+
+
+# 条件分岐
+# if hello == "Hello, world!" :
+#   print("Nice,greeting!")
+
+# if hello_length > 10 :
+#   print("good")
+# elif hello_length > 5 :
+#   print("soso")
+# else:
+#   print("not bad")
+
+
+# 関数
