1d437224885c666cd96b67f95733abcbfdfeb842
Sumio K <tap.miwo@gmail.com>
2022-11-09 06:43:37 +0900

    class

--- base.py
+++ base.py
@@ -74,7 +74,27 @@
 
 # with
 # open()
-with open('./sample.txt', 'r') as file:
-  print(file.name)
-  print(file.mode)
-  print(file.read())
+# with open('./sample.txt', 'r') as file:
+#   print(file.name)
+#   print(file.mode)
+#   print(file.read())
+
+
+# class
+class Card:
+  def __init__(self, date, user_name):
+    self.date = date
+    self.user_name = user_name
+  def message(self):
+    return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+
+date_a = "2020-01-01"
+user_name_a = "Taro"
+card_a = Card(date_a, user_name_a)
+
+
+date_b = '2020-01-03'
+user_name_b = "Kayoko"
+card_b = Card(date_b, user_name_b)
+print(card_b.message())
+# __init__ ...コンストラクタ関数(初回のみ呼ばれる関数)
