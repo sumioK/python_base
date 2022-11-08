6cd11729b1b996b6eda779e9cf31236291b64254
Sumio K <tap.miwo@gmail.com>
2022-11-09 06:52:46 +0900

    import

--- base.py
+++ base.py
@@ -81,20 +81,28 @@
 
 
 # class
-class Card:
-  def __init__(self, date, user_name):
-    self.date = date
-    self.user_name = user_name
-  def message(self):
-    return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+# class Card:
+#   def __init__(self, date, user_name):
+#     self.date = date
+#     self.user_name = user_name
+#   def message(self):
+#     return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
 
-date_a = "2020-01-01"
-user_name_a = "Taro"
-card_a = Card(date_a, user_name_a)
+# date_a = "2020-01-01"
+# user_name_a = "Taro"
+# card_a = Card(date_a, user_name_a)
 
 
-date_b = '2020-01-03'
-user_name_b = "Kayoko"
-card_b = Card(date_b, user_name_b)
-print(card_b.message())
+# date_b = '2020-01-03'
+# user_name_b = "Kayoko"
+# card_b = Card(date_b, user_name_b)
+# print(card_b.message())
 # __init__ ...コンストラクタ関数(初回のみ呼ばれる関数)
+
+
+# import
+import math
+import numpy as np
+
+np_list = [3, 5, 5, 2, 45, 64]
+print(np.sum(np_list))
